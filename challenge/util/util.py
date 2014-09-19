c16e78e0c9eba6ee9de8cf7eaa02315cc0071eac
Jon <jon_magal@yahoo.com.br>
2014-09-19 14:30:23 -0300

    Adding a function to concatenate two csv files.

--- challenge/util/util.py
+++ challenge/util/util.py
@@ -2,16 +2,33 @@
 '''
 Created on 28/05/2014
 
-@author: Jonathas Magalh������������������������������������������������������es
+@author: Jonathas Magalhães
 '''
 import json
 import os
+
+
 
 def read_sheet(file_name, fieldnames = None, delimiter = ",", quotechar = "\n"):
     from csv import DictReader
     
     reader = DictReader(open(file_name,'rb'), fieldnames = fieldnames, delimiter = delimiter, quotechar=quotechar)
     return reader
+
+def concatenate_csv(csv1_file, csv2_file, out_file):
+    fout = open(out_file, 'a')
+    with file(csv1_file,'r') as infile:
+        for line in infile:
+            fout.write(line)
+    header = True
+    with file(csv2_file,'r') as infile:
+        for line in infile:
+            if header:
+                header = False
+                continue # Skip the CSV header line
+            fout.write(line)        
+    fout.close()
+
 
 def save_sheet(file_name, content, title):        
     import csv
@@ -190,89 +207,4 @@ def create_subdataset(the_dataset_file, output_file, final = False):
         print e
         print todo
         return
-"""
-API IMDbPy - http://imdbpy.sourceforge.net/
 
-������ s������ instalar via easy_install ou pip:
-    sudo pip install IMDbPY
-    sudo easy_install IMDbPY
-
-Aqui eu tive que atualizar o arquivo setup_tools do python.
-Com a seguinte linha no terminal:
-    sudo easy_install -U setuptools 
-"""
-
-def get_movie_info(codigo_do_filme):
-    import imdb
-    ia = imdb.IMDb()
-    movie = ia.get_movie(codigo_do_filme)
-    return {'titulo': movie['title'], 'rating': movie['rating'], 'votes': movie['votes'], 'ano': movie['year'], 'genero': movie['genre'][0], 'pais': movie['countries'][0], 'idioma': movie['lang'][0], 'directors': movie['director']} 
-
-
-"""
-Below, a list of the main keys you can encounter, the type of the value
-returned by movieObject[key] and a short description/example:
-
-title; string; the "usual" title of the movie, like "The Untouchables".
-long imdb title; string; "Uncommon Valor (1983/II) (TV)"
-canonical title; string; the title in the canonical format,
-                         like "Untouchables, The".
-long imdb canonical title; string; "Patriot, The (2000)".
-year; string; the year of release or '????' if unknown.
-kind; string; one in ('movie', 'tv series', 'tv mini series', 'video game',
-                      'video movie', 'tv movie', 'episode')
-imdbIndex; string; the roman number for movies with the same title/year.
-director; Person list; a list of director's name (e.g.: ['Brian De Palma'])
-cast; Person list; list of actor/actress, with the currentRole instance
-                   variable set to a Character object which describe his
-                   role/duty.
-cover url; string; the link to the image of the poster.
-writer; Person list; list of writers ['Oscar Fraley (novel)']
-plot; list; list of plots and authors of the plot.
-rating; string; user rating on IMDb from 1 to 10 (e.g. '7.8')
-votes; string; number of votes (e.g. '24,101')
-runtimes; string list; in minutes ['119'] or something like ['USA:118',
-          'UK:116']
-number of episodes; int; number or episodes for a series.
-color info; string list; ["Color (Technicolor)"]
-countries; string list; production's country ['USA', 'Italy']
-genres; string list; one or more in (Action, Adventure, Adult, Animation,
-        Comedy, Crime, Documentary, Drama, Family, Fantasy, Film-Noir,
-        Horror, Musical, Mystery, Romance, Sci-Fi, Short, Thriller,
-        War, Western) and other genres defined by IMDb.
-akas; string list; list of aka for this movie
-languages; string list; list of languages
-"""
-    
-def create_csv_movie_info(the_sheet_file, output_file):
-    import sys
-    import codecs
-    encode =sys.stdin.encoding
-
-    todos   = read_sheet(the_sheet_file)
-    title   = ['id_movie', 'rating_imdb', 'number_of_votes', 'year', 'gender', 'country', 'language', 'directors']
-    content = []
-    
-    for todo in todos:
-        id_movie             = todo['id_move']
-        exists = False
-        for row in content:
-            if id_movie in row:
-                exists = True        
-        if not exists:
-            info = get_movie_info(id_movie)
-            rating_imdb = info['rating']
-            movie_votes = info['votes']
-            movie_year = info['ano']
-            movie_genre = info['genero']
-            movie_country = info['pais']
-            movie_lang = info['idioma']
-            
-            row = [id_movie, rating_imdb, movie_votes, movie_year, movie_genre, movie_country, movie_lang]
-            
-            for director in info['directors']:
-                row.append(director['name'].encode(encode))
-                
-            content.append(row)
-            print row
-            save_sheet(file_name = output_file, content = content, title = title)
